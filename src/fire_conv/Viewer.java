7313e1e92fef8a7ba425babf660eff74e3fb03bb
RromeroP <rubenromeroplanas@gmail.com>
2022-05-27 19:08:00 +0200

    v4 Needs more palettes. Fix black image on stop. Fix not being able to change filter if fire is on. Add wind/cooling map to the fire.

--- src/fire_conv/Viewer.java
+++ src/fire_conv/Viewer.java
@@ -6,6 +6,8 @@ package fire_conv;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
 /**
@@ -15,7 +17,7 @@ import java.awt.image.BufferedImage;
 public class Viewer extends Canvas {
 
     BufferedImage conv_bg;
-
+    BufferStrategy bs;
     BufferedImage bg;
     double[][] filter;
 
@@ -30,17 +32,27 @@ public class Viewer extends Canvas {
     public Viewer(Fire_Conv PRUEBA) {
         this.setBackground(Color.BLACK);
         this.PRUEBA = PRUEBA;
-
         viewerThread = new Thread(() -> {
+        paintViewer();
             while (running) {
+
                 setBackground(bg, filter);
 
+                try {
+                    Thread.sleep(1000 / 60);
+                } catch (InterruptedException ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
         });
     }
 
     public BufferedImage getConv_bg() {
         return conv_bg;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
     }
 
     public void setBackground(BufferedImage img_bg, double[][] img_filter) {
@@ -51,23 +63,50 @@ public class Viewer extends Canvas {
             this.running = true;
             viewerThread.start();
         }
+        
+        Graphics g = bs.getDrawGraphics();
 
         if (bg != null) {
-            if (first == true || !PRUEBA.fire.isRunning()) {
-                //Main Image
-                this.getGraphics().drawImage(bg, 0, (int) (this.getHeight() * 0.33), this.getWidth(), (int) (this.getHeight() * 0.67), null);
+
+            //Main Image
+            g.drawImage(bg, 0, (int) (this.getHeight() * 0.33), this.getWidth(), (int) (this.getHeight() * 0.67), null);
+
+            //Original Image
+            g.drawImage(bg, 0, 0, (int) (this.getWidth() * 0.33), (int) (this.getHeight() * 0.33), null);
+
+            //Conv Image
+            if (first == true) {
+                this.conv_bg = new Image_Conv(bg, filter).getNew_img();
                 first = false;
             }
-            //Original Image
-            this.getGraphics().drawImage(bg, 0, 0, (int) (this.getWidth() * 0.33), (int) (this.getHeight() * 0.33), null);
-            //Conv Image
-            this.conv_bg = new Image_Conv(bg, filter).getNew_img();
-            this.getGraphics().drawImage(conv_bg, (int) (this.getWidth() * 0.33), 0, (int) (this.getWidth() * 0.33), (int) (this.getHeight() * 0.33), null);
+            g.drawImage(conv_bg, (int) (this.getWidth() * 0.33), 0, (int) (this.getWidth() * 0.33), (int) (this.getHeight() * 0.33), null);
+
             //Fire
             BufferedImage fire_bg = PRUEBA.fire.getFlame_i();
-            this.getGraphics().drawImage(fire_bg, (int) (this.getWidth() * 0.67), 0, (int) (this.getWidth() * 0.34), (int) (this.getHeight() * 0.33), null);
+            g.drawImage(PRUEBA.fire.default_img, (int) (this.getWidth() * 0.67), 0, (int) (this.getWidth() * 0.34), (int) (this.getHeight() * 0.33), null);
+            g.drawImage(fire_bg, (int) (this.getWidth() * 0.67), 0, (int) (this.getWidth() * 0.34), (int) (this.getHeight() * 0.33), null);
+            g.drawImage(fire_bg, 0, (int) (this.getHeight() * 0.33), this.getWidth(), (int) (this.getHeight() * 0.67), null);
 
-            this.getGraphics().drawImage(fire_bg, 0, (int) (this.getHeight() * 0.33), this.getWidth(), (int) (this.getHeight() * 0.67), null);
+        }
+        
+        g.dispose();
+        
+        bs.show();
+    }
+
+    private void paintViewer() {
+        BufferStrategy buffers;
+        buffers = this.getBufferStrategy();
+        if (buffers == null) {
+            System.out.println("ERROR, kgd");
+            this.createBufferStrategy(3);
+            buffers = this.getBufferStrategy();
+        }
+
+        if (this.getParent() == null) {
+            System.out.println("ERROR, kgd 2");
+        } else {
+            this.bs = buffers;
         }
     }
 
